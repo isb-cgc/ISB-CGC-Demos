c6cdd496fb6dfb9825641b7de4acffb695b9a24a
John Phan <johnphan@f4520-1088475-m.gdit.com>
2021-09-16 09:45:07 -0400

    merge ML notebook and config

--- install.R
+++ install.R
@@ -1,3 +1,9 @@
 install.packages("readr")
 install.packages("bigrquery")
 install.packages("repr")
+install.packages("tidyr")
+install.packages("scales")
+
+install.packages("BiocManager")
+requireNamespace("BiocManager", quietly = TRUE)
+BiocManager::install(c("DESeq2"))
